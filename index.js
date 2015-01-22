8471a623873740491ef0344028c5763b6667d1d0
Ricky Vetter <ricky@socialtables.com>
2015-01-21 21:49:41 -0500

    add response

--- index.js
+++ index.js
@@ -30,5 +30,7 @@ http.createServer(function (request, response) {
       console.log(err, body);
     });
   });
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.end("Yo");
 }).listen(process.env.PORT || 5000);
 console.log('Server running at http://127.0.0.1:1337/');
